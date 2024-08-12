1d4058a8603ecf284847df4e8f33802675a64b20
Johnny Farid Montaño Angarita <ing.johnnyfaridmontanoangarita@gmail.com>
2024-08-12 17:30:37 -0500

    este es mi primer commit hecho en git

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Este es un codigo usando instrucciones de nivel superior.
+
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// Este es un codigo en el cual no usamos instrucciones de nivel superior en el codigo  esta era la manera tradicciona de hacerlo como siempre.
+
+/**
+    * asi se veria si no usamos instrucciones de nivel superior
+    * esta es la forma que se usa en proyectos mas grandes
+    * por mayor orden de codigo utilizamos todas las estructuras
+ */
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+			//Esperamos ejecucion y cierre de la consola.
+			Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Aprendiendo Csharp
+
+## Hola gente
+
+Esto es un ejemplo de subir un repertorio de GitHub
+
++ Aprendi Git
+- Cree mi cuenta de GitHub
+* Voy a subir mi primer repositorio a GitHub usando mi primeros comandos de Git
+
+![imagen de C#](image.png)
